82d75484ad7b9c48c6101b61756d555fd7ab633e
isadfrn <isadfrn@gmail.com>
2022-08-22 17:21:48 -0300

    feat: add sessions route

---
+++ server/src/controllers/SessionsController.js
@@ -0,0 +1,18 @@
+const knex = require('../database/knex');
+const AppError = require('../utils/AppError');
+
+class SessionsController {
+  async create(request, response) {
+    const { email, password } = request.body;
+
+    const user = await knex('users').where({ email }).first();
+
+    if (!user) {
+      throw new AppError('Invalid credentials', 401);
+    }
+
+    return response.json(user);
+  }
+}
+
+module.exports = SessionsController;
